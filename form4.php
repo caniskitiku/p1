7e9f47f7b97c552acb6b6b0914fca4f84bc22242
caniskitiku <1800001312@stu.iku.edu.tr>
2021-03-20 11:55:57 +0300

    Form4.php

---
+++ form4.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form1</title>
+</head>
+<body>
+    <form method="Post">
+    <p>User Input: <Input type="text" name="Input" placeholder="Enter Something"></Input>
+    </p>
+    <input type="submit">
+    </form>
+    <pre>
+   
+    $_POST
+    <?php 
+    print_r($_POST)
+    ?>
+    </pre>
+</body>
+</html>
